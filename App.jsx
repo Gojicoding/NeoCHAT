ec8047acef39491a54b12be9dbfcf6b025a66f27
Eugene Hardimon <lilgenoplays@gmail.com>
2025-08-26 00:24:50 -0500

    updated

--- App.jsx
+++ App.jsx
@@ -55,8 +55,27 @@ function SignOut(){
 }
 
 
-function ChatRoom() {}
+function ChatRoom() {
 
+       const messagesRef = firestore.collection('messages');
+       const query = messagesRef.orderBy('createdAt').limit(25);
+
+       const [messages] = useCollectionData(query, {idField: 'id'});
+       
+       return (
+              <>
+              <div>
+                     {messages && messages.map(msg => <ChatMessages key={msg.id} message={msg}/> )}
+              </div>
+              </>
+       )
+}
+
+function ChatMessage(props) {
+     const { text, uid } = props.message;  
+
+     return <p>{text}  </p>
+}
 
 
 export default App,
